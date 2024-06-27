603231c98f4f0093cc1cdd694ccbd030261ea10b
Jay22996 <jay00289@gmail.com>
2024-06-27 18:08:48 +0530

    all done

--- controller/main.js
+++ controller/main.js
@@ -1,17 +1,22 @@
 // controllers/dataController.js
 
-const db = require('../model/db');
+const db = require("../model/db");
 
 exports.getData = async (req, res) => {
   try {
     const pool = await db.poolPromise;
-    const result = await pool.request().query('SELECT * FROM AcGrp'); 
+    const result = await pool
+      .request()
+      .query(
+        " SELECT g.acid, SUM(g.totalmeter) as totalMeterSum, a.acname FROM GRY_Inward g INNER JOIN ac a ON g.acid = a.acid GROUP BY g.acid, a.acname"
+      );
+    console.log(result.recordset.length);
     res.status(200).send({
-        status:"done",
-        data:result
+      status: "done",
+      data: result,
     });
   } catch (err) {
-    console.error('SQL error', err);
-    res.status(500).send('Server Error');
+    console.error("SQL error", err);
+    res.status(500).send("Server Error");
   }
 };
