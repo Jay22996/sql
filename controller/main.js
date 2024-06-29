2d6678b3dcdd4f7cf94747d7961e56b2dfa4e2da
Jay22996 <jay00289@gmail.com>
2024-06-29 13:17:42 +0530

    add done

--- controller/main.js
+++ controller/main.js
@@ -20,10 +20,38 @@ exports.getData = async (req, res) => {
     res.status(200).json({
       status: "done",
       data: result.recordset,
-      subtotal: subtotal // Optionally include subtotal in the response
+      subtotal: subtotal
     });
   } catch (err) {
     console.error("SQL error", err);
     res.status(500).send("Server Error");
   }
 };
+
+exports.chart = async (req, res) => {
+  try {
+    const pool = await db.poolPromise;
+    const query = `
+      SELECT TOP 10 g.acid, SUM(g.totalmeter) as totalMeterSum, a.acname 
+      FROM GRY_Inward g 
+      INNER JOIN ac a ON g.acid = a.acid 
+      GROUP BY g.acid, a.acname
+      ORDER BY totalMeterSum DESC
+    `;
+    const result = await pool.request().query(query);
+
+    // Calculate subtotal using Array.reduce method
+    const subtotal = result.recordset.reduce((acc, record) => acc + record.totalMeterSum, 0);
+    console.log(subtotal);
+
+    res.status(200).json({
+      status: "done",
+      data: result.recordset,
+      subtotal: subtotal
+    });
+  } catch (err) {
+    console.error("SQL error", err);
+    res.status(500).send("Server Error");
+  }
+};
+
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,7 @@ const router = express.Router();
 const dataController = require('../controller/main');
 
 router.get('/data', dataController.getData);
+router.get('/', dataController.chart);
+
 
 module.exports = router;
